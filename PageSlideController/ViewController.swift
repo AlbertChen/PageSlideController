c7258165af9fa6fddddec3d6e5b7313551bed152
chenyiliang <yiliang.chen@gocmx.com>
2020-10-28 23:45:10 +0800

    Initial commit

---
+++ PageSlideController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageSlideController
+//
+//  Created by Chen Yiliang on 2020/10/28.
+//  Copyright © 2020 Chen Yiliang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
